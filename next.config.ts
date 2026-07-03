7509ce9f7a5624aa308f93c9db8851141c62ee58
Yashkadam04 <166133037+Yashkadam04@users.noreply.github.com>
2026-07-03 12:10:10 +0530

    Enable unoptimized images in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/yash-portfolio",
   assetPrefix: "/yash-portfolio/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
